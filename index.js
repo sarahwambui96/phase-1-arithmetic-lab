e62f49949e944e49598d77f931e8f2754260adf7
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-13 19:49:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+//multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// random
+const number = 22;
+const random = number;
+
+// mod
+const num3 = 24;
+const num4 = 5;
+const mod = 24 % 5;
+
+//max
+const max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
